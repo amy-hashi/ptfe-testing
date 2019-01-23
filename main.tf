9bbd6be27b8fe38420a9e19c3bd4acb91bd49305
Amy Brown <32877781+amy-hashi@users.noreply.github.com>
2019-01-23 15:11:45 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 resource "aws_instance" "amy-testing" {
   ami           = "ami-be7753db"
   instance_type = "t2.micro"
-  count = 2
+  count = 0
   tags {
     "Owner" = "Amy Brown"
 #    "TTL" = "30"
