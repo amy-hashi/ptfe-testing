b9cd302a9db0edd4e1c337209f35fe20e3011ce9
Amy Brown <32877781+amy-hashi@users.noreply.github.com>
2021-05-04 15:59:18 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
+  access_key = var.access_key
+  secret_key = var.secret_key
   region     = "us-east-2"
 }
 
 resource "aws_instance" "amy-testing" {
   ami           = "ami-be7753db"
-  instance_type = "${var.instance-type}"
-  count = "${var.count}"
+  instance_type = var.instance-type
+  count = var.count
   tags {
     "Owner" = "Amy Brown"
   }
