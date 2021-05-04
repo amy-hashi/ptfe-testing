cad662a7b84799cdfc1985bd0dba5198132406a0
Amy Brown <32877781+amy-hashi@users.noreply.github.com>
2021-05-04 16:05:21 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,8 @@ resource "aws_instance" "amy-testing" {
   ami           = "ami-be7753db"
   instance_type = var.instance-type
   count = var.ec2count
-  tags {
+  
+  tags = {
     Owner = "Amy Brown"
   }
 }
