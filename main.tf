96202d29ce191dc2d7e8a8fccee02420b9e361cb
Amy Brown <32877781+amy-hashi@users.noreply.github.com>
2021-10-19 15:15:28 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,8 +5,21 @@ provider "aws" {
   region     = "us-east-2"
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+      name   = "name"
+      values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  filter {
+      name = "virtualization-type"
+      values = ["hvm"]
+  }
+  owners = ["099720109477"]
+}
+
 resource "aws_instance" "amy-testing" {
-  ami           = "ami-be7753db"
+  ami           = data.aws_ami.ubuntu.image_id
   instance_type = var.instance-type
   count = var.ec2count
   
